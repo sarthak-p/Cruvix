bf1b632da7ce60256b83d218b4dac79e12bb16ed
Sarthak Patipati <pusarthak@gmail.com>
2024-06-24 21:19:14 -0500

    Update README.md

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253eeae9ec6bad93a0a327f6fadcb09b7f2cd175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b43183fd9b82aa8297aa2a18c71cad70b967ef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
