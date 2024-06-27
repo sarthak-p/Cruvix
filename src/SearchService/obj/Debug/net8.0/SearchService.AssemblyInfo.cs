61868aba8307d9a82be1d6d891e1b87915e075fa
Sarthak Patipati <pusarthak@gmail.com>
2024-06-27 15:28:08 -0500

    Used message retries

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5020df9956b554e6e8e42f3153f71b659b040234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c857d3742b857edc30c164ec64cf20a4a790d7f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
