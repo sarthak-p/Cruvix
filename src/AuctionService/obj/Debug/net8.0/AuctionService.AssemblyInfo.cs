3cf8c270ce0a43e1ed067f6eb25ab19bae9c93f4
Sarthak Patipati <pusarthak@gmail.com>
2024-06-28 17:42:02 -0500

    Configured the auth endpoints on the resource server

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f1db62c8f008a287501dc1f14933763f3da221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9a9ad433d5bb74ad2ec0cb7750d8b60a26c610")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f1db62c8f008a287501dc1f14933763f3da221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9a9ad433d5bb74ad2ec0cb7750d8b60a26c610")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
