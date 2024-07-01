03d378e0082ea34427d6dad22a5fe2b4cc70e57c
Sarthak Patipati <pusarthak@gmail.com>
2024-07-01 10:53:12 -0500

    Dockerized the Identity Service

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1e16f544518a23be47d3254d7802366d1d023d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd43fb34f413ad9e81c06e618d92841aae9ca470")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
