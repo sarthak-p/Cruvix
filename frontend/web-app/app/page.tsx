c4a67bce62ac95f80d5f985b4e2c87866a433da2
Sarthak Patipati <pusarthak@gmail.com>
2024-07-02 12:26:35 -0500

    Created a nav bar

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h3 className='text -2xl font-semibold'>
-        Cruvix App!
-      </h3>
+      Content goes here!
     </div>
   )
 }
