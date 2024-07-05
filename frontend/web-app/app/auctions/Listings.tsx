25532719baaaa07ef5c34e3d532ad50efada8a20
Sarthak Patipati <pusarthak@gmail.com>
2024-07-04 22:14:17 -0500

    Added a component to display when zero results

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string'
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
 
@@ -36,18 +37,24 @@ export default function Listings() {
 
   if (!data) return<h3>Loading...</h3>
 
-
   return ( 
     <>
       <Filters />
-      <div className='grid grid-cols-4 gap-6'>
-        {data.results.map((auction) => (
-          <AuctionCard auction={auction} key={auction.id} />
-        ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />  
-      </div>
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ) : (
+          <>
+            <div className='grid grid-cols-4 gap-6'>
+            {data.results.map((auction) => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+            </div>
+            
+            <div className='flex justify-center mt-4'>
+              <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />  
+            </div>
+          </>
+      )}
     </>
   );
 }
