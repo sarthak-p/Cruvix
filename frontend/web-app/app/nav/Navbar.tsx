781270bb9ee3d8aa5d403b324c865fad14750565
Sarthak Patipati <pusarthak@gmail.com>
2024-07-09 14:34:15 -0500

    Populated the User actions dropdown

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -10,10 +10,10 @@ export default async function Navbar() {
   const user = await getCurrentUser(); 
   return (
       <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
-          <Logo />
+      <Logo />
       <Search />
       {user ? (
-        <UserActions />
+        <UserActions user={user} />
       ) : (
           <LoginButton />
       ) }
