656d2d7e589421e7a2ddb2a629f6e537857a67a8
Sarthak Patipati <pusarthak@gmail.com>
2024-07-09 15:49:01 -0500

    Got the access token to use to authenticate to the resource server

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Heading from '../components/Heading';
-import { getSession } from '../actions/authActions';
+import { getSession, getTokenWorkaround } from '../actions/authActions';
 import AuthTest from './AuthTest';
 
 export default async function Session() {
 
-    const session = await getSession(); 
+  const session = await getSession(); 
+  const token = await getTokenWorkaround(); 
+  
   return (
     <div>
           <Heading title='Session dashboard' />
@@ -16,6 +18,10 @@ export default async function Session() {
       </div>
       <div className='mt-4'>
         <AuthTest />
+      </div>
+      <div className='bg-green-200 border-2 border-blue-500'>
+              <h3 className='text-lg'>Token data</h3>
+              <pre className='overflow-auto'>{JSON.stringify(token, null, 2)}</pre>
       </div>
     </div>
   )
