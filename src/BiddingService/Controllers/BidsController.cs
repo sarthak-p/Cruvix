81ac21cff4fd5420080fce774b50827fb854a1e2
Sarthak Patipati <pusarthak@gmail.com>
2024-07-18 15:58:14 -0500

    Added the get bids endpoint

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -59,4 +59,16 @@ public class BidsController : ControllerBase
         return Ok(bid);
 
     }
+
+    [HttpGet("{auctionId}")]
+    public async Task<ActionResult<List<Bid>>> GetBidsForAuction(string auctionId)
+    {
+        var bids = await DB.Find<Bid>()
+            .Match(a => a.AuctionId == auctionId)
+            .Sort(b => b.Descending(x => x.BidTime))
+            .ExecuteAsync();
+
+        return bids;
+    }
+
 }
