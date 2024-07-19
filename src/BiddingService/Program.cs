f56271d00e21fe879f573717ef43a579ca46bfad
Sarthak Patipati <pusarthak@gmail.com>
2024-07-19 18:06:23 -0500

    Added the DTOs and Automapper

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -37,6 +37,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
 
 var app = builder.Build();
 
--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ac21cff4fd5420080fce774b50827fb854a1e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1ec6ecdd2bec88300aaae10cd6f69a03a97f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
