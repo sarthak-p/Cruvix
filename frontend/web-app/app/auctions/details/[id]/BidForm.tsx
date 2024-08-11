059e822893b8109d18945f7716f342de4ea340d0
Sarthak Patipati <pusarthak@gmail.com>
2024-08-11 10:05:27 -0500

    Prevented low bids

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -18,6 +18,10 @@ export default function BidForm({ auctionId, highBid }: Props) {
     const addBid = useBidStore((state) => state.addBids);
 
     function onSubmit(data: FieldValues) {
+        if (data.amount <= highBid) {
+            reset(); 
+            return toast.error('Bid must be at least $' + numberWithCommas(highBid + 1));
+        }
         placeBidForAuction(auctionId, +data.amount).then((bid) => {
             if (bid.error) {
                 throw bid.error
