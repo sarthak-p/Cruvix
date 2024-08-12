008d6f6c51189d879a5b98071acb91e96b7137ce
Sarthak Patipati <pusarthak@gmail.com>
2024-08-12 08:52:29 -0500

    Created the Dockerfile for Next.js

--- frontend/web-app/next.config.mjs
+++ frontend/web-app/next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true
-    },
     images: {
         domains: [
             'cdn.pixabay.com'
         ]
-    }
+    },
+    output: 'standalone'
 };
 
 export default nextConfig;
